e7a20b12e968b9e3158ddef2c898dbdd7ce72b88
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2025-05-07 18:36:28 +0700

    updatr

--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -7,7 +7,7 @@ export const createToken = (user, res) => {
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "strict",
         secure: true
     })
 }
